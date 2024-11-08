84835a53d30ca84663d081701af60cd83462b8cd
Antonio Zea Jr <zeajantonio@gmail.com>
2024-11-07 21:10:40 -0500

    refactor(to_camel_case.c): simplify character case conversion and remove unused functions

--- code_competitions/codewars/C/ConvertStringToCamelCase/src/to_camel_case.c
+++ code_competitions/codewars/C/ConvertStringToCamelCase/src/to_camel_case.c
@@ -1,36 +1,13 @@
+#include <ctype.h>
 #include <stdlib.h>
-
-char uppercase(const char *character);
-char lowercase(const char *character);
 
 void to_camel_case(const char *text, char *camel) {
 
   for (int i = 0; text[i]; i++) {
     if (text[i] == '_' || text[i] == '-') {
-      i++;
-      *camel = uppercase(text + i);
-    } else {
-      if (i == 0) {
-        *camel = text[i];
-      } else {
-        *camel = lowercase(text + i);
-      }
-    }
-    camel++;
+      *(camel++) = toupper(text[++i]);
+    } else
+      *(camel++) = text[i];
   }
   *camel = '\0';
-}
-
-char uppercase(const char *character) {
-  if (*character >= 'a' && *character <= 'z')
-    return *character - ' ';
-  else
-    return *character;
-}
-
-char lowercase(const char *character) {
-  if (*character >= 'A' && *character <= 'Z')
-    return *character + ' ';
-  else
-    return *character;
 }
